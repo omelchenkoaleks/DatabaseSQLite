c0c262b6803f1df128e9e40da6d6104c1c144fc9
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-09-25 10:53:46 +0300

    транзакции ускоряют добавление данных, SQLiteStatement - еще больше ускроряет ...

--- app/src/main/java/com/omelchenkoaleks/databasesqlite/_006_insert_transactions/InsertTransactionActivity.java
+++ app/src/main/java/com/omelchenkoaleks/databasesqlite/_006_insert_transactions/InsertTransactionActivity.java
@@ -1,7 +1,7 @@
 package com.omelchenkoaleks.databasesqlite._006_insert_transactions;
 
-import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +12,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.omelchenkoaleks.databasesqlite.R;
 
 public class InsertTransactionActivity extends AppCompatActivity implements View.OnClickListener {
-    private static final String TAG = "InsertTransaction";
 
     private static final String DB_NAME = "transactionDB";
     private static final String TABLE_NAME = "insertDB";
@@ -49,12 +48,44 @@ public class InsertTransactionActivity extends AppCompatActivity implements View
     }
 
     private void insertRecords() {
-        for (int i = 0; i < 1000; i++) {
-            ContentValues contentValues = new ContentValues();
-            contentValues.put("FirstNumber", i);
-            contentValues.put("SecondNumber", i);
-            contentValues.put("Result", i * i);
-            mDatabase.insert(TABLE_NAME, null, contentValues);
+//        for (int i = 0; i < 1000; i++) {
+//            ContentValues contentValues = new ContentValues();
+//            contentValues.put("FirstNumber", i);
+//            contentValues.put("SecondNumber", i);
+//            contentValues.put("Result", i * i);
+//            mDatabase.insert(TABLE_NAME, null, contentValues);
+//        }
+
+        // используем транзакции - это ускорит выполнение кода в несколько раз!!!
+//        mDatabase.beginTransaction();
+//        try {
+//            for (int i = 0; i < 1000; i++) {
+//                ContentValues contentValues = new ContentValues();
+//                contentValues.put("FirstNumber", i);
+//                contentValues.put("SecondNumber", i);
+//                contentValues.put("Result", i * i);
+//                mDatabase.insert(TABLE_NAME, null, contentValues);
+//            }
+//            mDatabase.setTransactionSuccessful();
+//        } finally {
+//            mDatabase.endTransaction();
+//        }
+
+        // еще быстрее выполнится, если использовать SQLiteStatement (множественная вставка) !!!
+        String sql = "INSERT INTO " + TABLE_NAME + " VALUES(?,?,?);";
+        SQLiteStatement statement = mDatabase.compileStatement(sql);
+        mDatabase.beginTransaction();
+        try {
+            for (int i = 0; i < 1000; i++) {
+                statement.clearBindings();
+                statement.bindLong(1, i);
+                statement.bindLong(2, i);
+                statement.bindLong(3, i * i);
+                statement.execute();
+            }
+            mDatabase.setTransactionSuccessful();
+        } finally {
+            mDatabase.endTransaction();
         }
     }
 
